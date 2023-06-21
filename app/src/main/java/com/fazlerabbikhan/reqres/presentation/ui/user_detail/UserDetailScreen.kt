303e0124f3d56fd5e0b63996162e57db62bf9ed0
Fazle Rabbi Khan <fazlerabbi.frk@gmail.com>
2023-06-21 17:41:25 +0600

    Card added

--- app/src/main/java/com/fazlerabbikhan/reqres/presentation/ui/user_detail/UserDetailScreen.kt
+++ app/src/main/java/com/fazlerabbikhan/reqres/presentation/ui/user_detail/UserDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.fazlerabbikhan.reqres.presentation.ui.user_detail
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,10 +21,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.google.accompanist.flowlayout.FlowRow
 
 @Composable
@@ -29,44 +36,62 @@ fun UserDetailScreen(
     viewModel: UserDetailViewModel = hiltViewModel()
 ) {
     val state = viewModel.state.value
-    Box(modifier = Modifier.fillMaxSize()) {
+    Box(modifier = Modifier.fillMaxSize()
+        .padding(horizontal = 16.dp),
+        contentAlignment = Alignment.Center) {
         state.user?.let { user ->
-            Column(
+            Card(
                 modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "First Name: ${user.first_name}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.weight(8f)
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Last Name: ${user.last_name}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.weight(8f)
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Email: ${user.email}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.weight(8f)
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Support: ${user.supportUrl}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.weight(8f)
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = "Description: ${user.supportText}",
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.weight(8f)
-                )
-            }
+                content = {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Image(
+                            painter = // Apply transformations if needed
+                            rememberAsyncImagePainter(
+                                ImageRequest.Builder(LocalContext.current).data(data = user.avatar)
+                                    .apply<ImageRequest.Builder>(block = fun ImageRequest.Builder.() {
+                                        transformations(CircleCropTransformation()) // Apply transformations if needed
+                                    }).build()
+                            ),
+                            contentDescription = "Image Description",
+                            modifier = Modifier.size(200.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "First Name: ${user.first_name}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Last Name: ${user.last_name}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Email: ${user.email}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Support: ${user.supportUrl}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(
+                            text = "Description: ${user.supportText}",
+                            style = MaterialTheme.typography.bodyMedium,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                }
+            )
         }
-        if(state.error.isNotBlank()) {
+        if (state.error.isNotBlank()) {
             Text(
                 text = state.error,
                 color = MaterialTheme.colorScheme.error,
@@ -77,7 +102,7 @@ fun UserDetailScreen(
                     .align(Alignment.Center)
             )
         }
-        if(state.isLoading) {
+        if (state.isLoading) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         }
     }
--- app/src/main/java/com/fazlerabbikhan/reqres/presentation/ui/user_list/components/UserListItem.kt
+++ app/src/main/java/com/fazlerabbikhan/reqres/presentation/ui/user_list/components/UserListItem.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
@@ -30,7 +35,8 @@ fun UserListItem(
             .fillMaxWidth()
             .clickable { onItemClick(user) }
             .padding(20.dp),
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.spacedBy(40.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Image(
             painter = // Apply transformations if needed
@@ -44,9 +50,11 @@ fun UserListItem(
             modifier = Modifier.size(50.dp)
         )
         Text(
+            textAlign = TextAlign.Center,
             text = "${user.first_name} ${user.last_name}",
             style = MaterialTheme.typography.bodyLarge,
             overflow = TextOverflow.Ellipsis
         )
     }
+    Divider()
 }
