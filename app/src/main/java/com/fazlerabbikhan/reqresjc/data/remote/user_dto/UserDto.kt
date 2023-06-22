26ca8d076274bdb440dcb1a61eab5d7cfec93a8f
Fazle Rabbi Khan <fazlerabbi.frk@gmail.com>
2023-06-22 15:34:08 +0600

    optimized

--- app/src/main/java/com/fazlerabbikhan/reqresjc/data/remote/user_dto/UserDto.kt
+++ app/src/main/java/com/fazlerabbikhan/reqresjc/data/remote/user_dto/UserDto.kt
@@ -8,25 +8,3 @@ data class UserDto(
     val total: Int,
     val total_pages: Int
 )
-
-//fun UserDto.toUser(): List<User> {
-//    return data.map { user ->
-//        User(
-//            id = user.id,
-//            avatar = user.avatar,
-//            first_name = user.first_name,
-//            last_name = user.last_name,
-//            email = user.email
-//        )
-//    }
-//}
-
-//fun UserDto.toUser(): User {
-//    return User(
-//        id = data.map { it.id }[0],
-//        avatar = data.map { it.avatar }[0],
-//        first_name = data.map { it.first_name }[0],
-//        last_name = data.map { it.last_name }[0],
-//        email = data.map { it.email }[0]
-//    )
-//}
--- app/src/main/java/com/fazlerabbikhan/reqresjc/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/fazlerabbikhan/reqresjc/presentation/ui/MainActivity.kt
@@ -41,19 +41,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ReqresTheme {
-//        Greeting("Android")
-//    }
-//}
